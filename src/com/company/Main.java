892c92cf3e49bff630a3a96804acaa1fb367d7f3
Andrius <andriuscere@gmail.com>
2018-01-30 20:43:35 +0200

    Uzduotis12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ivedimas();
+        System.out.println("Jusu KMI: " + skaiciavimas.KMIskaiciuokle(a,b));
+
     }
+    static float a;
+    static float b;
+
+    public static void ivedimas(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite savo svori KG");
+        a = sc.nextFloat();
+        System.out.println("Iveskite savo ugi M");
+        b = sc.nextFloat();
+    }
+
 }
---
+++ src/com/company/skaiciavimas.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class skaiciavimas {
+    public static float KMIskaiciuokle(float a, float b){
+        return (a/(b*b));
+    }
+
+}
